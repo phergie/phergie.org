b82db75fbece9104dc0bc5ed2be42665e3680d94
Joe Ferguson <joe@joeferguson.me>
2015-08-31 20:25:42 -0500

    Update our publish scripts to deploy to new server

--- publish-dev.sh
+++ publish-dev.sh
@@ -9,5 +9,5 @@ if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
 # Add --delete right before "output_prod" to have rsync remove files that are
 # deleted locally from the destination too. See README.md for an example.
-rsync -avze 'ssh -p 22' output_prod/ phergie@phergie.org:public_html
+rsync -avze 'ssh -p 22' output_prod/ phergie@phergie.org:domains/dev.phergie.org/public_html
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
